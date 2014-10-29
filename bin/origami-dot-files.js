8e4b62305a6bf23cde4a516a001179a0ccf407e0
Matthew Andrews <matt@mattandre.ws>
2014-10-29 23:58:41 +0000

    Initial commit

---
+++ bin/origami-dot-files.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+'use strict';
+
+require('es6-promise').polyfill();
+var denodeify = require('denodeify');
+var exec = denodeify(require('child_process').exec);
+
+var argv = require('minimist')(process.argv.slice(2));
+var promise = Promise.resolve();
+
+if (argv._.indexOf('install') !== -1) {
+
+	// -g downloads to home directory (*nix only)
+	var directory = argv._.indexOf('-g') ? process.env.HOME : promise.cwd();
+	console.log("Downloading .jshintrc & .editorconfig to " + directory);
+	Promise.all([
+		exec('curl https://raw.githubusercontent.com/Financial-Times/origami-build-tools/master/config/jshint.json -o .jshintrc', { cwd: directory }),
+		exec('curl https://raw.githubusercontent.com/Financial-Times/origami-build-tools/master/config/.editorconfig -o .editorconfig', { cwd: directory })
+	])
+		.then(function() {
+			console.log("Updated .jshintrc & .editorconfig");
+		}, function(error) {
+			console.log("An error has occurred: ", error);	
+		});
+}
